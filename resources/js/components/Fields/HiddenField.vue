33c3e67acd82f9f18987058bc1b1addd6bf3eb2b
Johan Montenij <johan@comfycoders.nl>
2025-12-28 14:27:53 +0100

    Add 'id' to hidden field + error feedback

--- resources/js/components/Fields/HiddenField.vue
+++ resources/js/components/Fields/HiddenField.vue
@@ -2,11 +2,16 @@
 import type { Field } from '../../types/form-builder'
 
 withDefaults(defineProps<Field>(), {
-  name: undefined,
-  modelValue: '',
+    error: undefined,
+    modelValue: '',
+    name: undefined,
 })
 </script>
 
 <template>
-  <input type="hidden" :name="name" :value="(modelValue as any) ?? ''" />
+    <input type="hidden" :name="name" :id="name" :value="(modelValue as any) ?? ''" />
+    <p v-if="error" class="text-sm text-red-600">
+        <span v-if="Array.isArray(error)">{{ error[0] }}</span>
+        <span v-else>{{ error }}</span>
+    </p>
 </template>
